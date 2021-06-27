48c1d6bfab254b9aba89d64f4450362eb53e7ce8
VarunPotti <potti.varun07@gmail.com>
2021-06-27 17:20:33 +0000

    Deploying to gh-pages from  @ 95f7c260e39368efaaaa48fabc4b69941c69bcef 🚀

---
+++ _next/static/Vw4Bzbk4kai6AaPyYyg24/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/659bbbff90be71a0fde5.css","static/chunks/pages/index-fdd316fbfcd2c7bee8df.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
